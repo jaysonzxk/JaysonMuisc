7cd45ae6b14ea61128ae47fb0dba0f0f23f0202e
jayson <1344173512@qq.com>
2022-07-14 14:03:48 +0800

    ok

---
+++ src/main/java/com/jayson/jaysonmuisc/JaysonMuiscApplication.java
@@ -0,0 +1,13 @@
+package com.jayson.jaysonmuisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaysonMuiscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaysonMuiscApplication.class, args);
+    }
+
+}
